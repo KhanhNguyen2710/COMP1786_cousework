3ed05822414fc90c5c67e76987dfec54571594e8
Khánh Nguyễn <hanaaimi304@gmail.com>
2023-11-14 21:45:16 +0700

    add date, spinner

---
+++ app/src/main/java/com/example/myapplication/DatePicker/DatePickerDialogFragment.java
@@ -0,0 +1,40 @@
+package com.example.myapplication.DatePicker;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.myapplication.Fragment.AddFragment;
+
+import java.util.Calendar;
+
+public class DatePickerDialogFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        // Sử dụng ngày hiện tại làm giá trị mặc định
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Tạo một đối tượng DatePickerDialog và trả về nó
+        return new DatePickerDialog(requireContext(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        // Gửi ngày được chọn về fragment cha (AddFragment)
+        ((AddFragment) getParentFragment()).updateDateTextView(year, month, dayOfMonth);
+    }
+
+}
--- app/src/main/java/com/example/myapplication/Fragment/AddFragment.java
+++ app/src/main/java/com/example/myapplication/Fragment/AddFragment.java
@@ -1,27 +1,39 @@
 package com.example.myapplication.Fragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.room.Room;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplication.Database.HikeDatabase;
+import com.example.myapplication.DatePicker.DatePickerDialogFragment;
 import com.example.myapplication.Models.Hike;
 import com.example.myapplication.R;
 import com.example.myapplication.dao.HikeDao;
 
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class AddFragment extends Fragment {
-    private View view;
+
+private View view;
     private HikeDatabase hikeDatabase;
     public AddFragment() {
         // Required empty public constructor
@@ -29,42 +41,62 @@ public class AddFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_add, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_add, container, false);
 
         hikeDatabase = Room.databaseBuilder(getActivity().getApplicationContext(), HikeDatabase.class, "sqlite_example_db")
                 .allowMainThreadQueries() // For simplicity, don't use this in production
                 .build();
 
+        view = rootView;
 
+// Spinner
+        Spinner spinnerLevel = view.findViewById(R.id.spinnerLevel);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(requireContext(), R.array.levels_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerLevel.setAdapter(adapter);
 
+//date
+
+//        TextView Date = view.findViewById(R.id.Date);
+        TextView Date = view.findViewById(R.id.Date);
+        Date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Mở DatePickerDialogFragment khi nhấp vào TextView Date
+                showDatePickerDialog();
+            }
+        });
+
+//
+        EditText editName = view.findViewById(R.id.editName);
+        EditText editLocation = view.findViewById(R.id.editLocation);
+        EditText editDescription = view.findViewById(R.id.editDescription);
+        EditText editLength = view.findViewById(R.id.editLength);
+        RadioGroup radioGroupParking = view.findViewById(R.id.radioGroupParking);
+//
         Button buttonSubmit = view.findViewById(R.id.buttonSubmit);
         buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                saveToDatabase();
-                EditText editName = view.findViewById(R.id.editName);
-                EditText editLocation = view.findViewById(R.id.editLocation);
-                EditText editDate = view.findViewById(R.id.editDate);
-                EditText editDescription = view.findViewById(R.id.editDescription);
-                EditText editLength = view.findViewById(R.id.editLength);
-                EditText editLevel = view.findViewById(R.id.editLevel);
-                RadioGroup radioGroupParking = view.findViewById(R.id.radioGroupParking);
-
+// lấy văn bản
                 String name = editName.getText().toString();
                 String location = editLocation.getText().toString();
-                String date = editDate.getText().toString();
+                String date = Date.getText().toString();
                 String description = editDescription.getText().toString();
                 String length = editLength.getText().toString();
-                String level = editLevel.getText().toString();
+                String selectedLevel = spinnerLevel.getSelectedItem().toString();
+
                 int selectedRadioButtonId = radioGroupParking.getCheckedRadioButtonId();
                 boolean parkingAvailable = (selectedRadioButtonId == R.id.checkYes);
+
 
                 Hike hike = new Hike();
                 hike.name= name;
                 hike.location= location;
                 hike.date= date;
                 hike.length= length;
-                hike.level= level;
+                hike.level = selectedLevel;
                 hike.description= description;
                 hike.parkingAvailable= parkingAvailable;
 
@@ -77,34 +109,22 @@ public class AddFragment extends Fragment {
             }
         });
 
-
-        return view;
+        return rootView;
     }
-//    private void saveToDatabase() {
-//        EditText editName = view.findViewById(R.id.editName);
-//        EditText editLocation = view.findViewById(R.id.editLocation);
-//        EditText editDate = view.findViewById(R.id.editDate);
-//        EditText editDescription = view.findViewById(R.id.editDescription);
-//        EditText length = view.findViewById(R.id.length);
-//        EditText level = view.findViewById(R.id.level);
-//        RadioGroup radioGroupParking = view.findViewById(R.id.radioGroupParking);
-//
-//        String name = editName.getText().toString();
-//        String location = editLocation.getText().toString();
-//        String date = editDate.getText().toString();
-//        String description = editDescription.getText().toString();
-//        String hikeLength = length.getText().toString();
-//        String hikeLevel = level.getText().toString();
-//        int selectedRadioButtonId = radioGroupParking.getCheckedRadioButtonId();
-//        boolean parkingAvailable = (selectedRadioButtonId == R.id.checkYes);
-//
-//        Hike hike = new Hike();
-//        hike.name= name;
-//        // Set properties of the Hike object based on the user input
-//
-//        long hike_id = hikeDatabase.hikeDao().insertHike(hike);
-//
-//        Toast.makeText(requireContext(), "Hike has been created with id: " + hike_id,
-//                Toast.LENGTH_LONG).show();
-//    }
+
+
+    private void showDatePickerDialog() {
+        DatePickerDialogFragment datePickerFragment = new DatePickerDialogFragment();
+        datePickerFragment.show(getChildFragmentManager(), "datePicker");
+    }
+
+    // Cập nhật TextView Date sau khi chọn ngày
+    public void updateDateTextView(int year, int month, int day) {
+        TextView Date = view.findViewById(R.id.Date);
+        String formattedDate = String.format(Locale.getDefault(), "%02d/%02d/%04d", month + 1, day, year);
+        Date.setText(formattedDate);
+    }
+
+
+
 }
